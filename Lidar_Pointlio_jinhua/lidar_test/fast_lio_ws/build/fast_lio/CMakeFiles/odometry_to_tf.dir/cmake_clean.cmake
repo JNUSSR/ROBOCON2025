a7ecf491c927762aeee671cb5ba06b865c5b0f32
UcWc1729 <954450010@qq.com>
2025-07-30 00:43:39 +0800

    Update files and directories

---
+++ Lidar_Pointlio_jinhua/lidar_test/fast_lio_ws/build/fast_lio/CMakeFiles/odometry_to_tf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odometry_to_tf.dir/src/odometry_to_tf.cpp.o"
+  "CMakeFiles/odometry_to_tf.dir/src/odometry_to_tf.cpp.o.d"
+  "odometry_to_tf"
+  "odometry_to_tf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odometry_to_tf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lidar_Pointlio_jinhua/unilidar_sdk2-2.0.4/unitree_lidar_ros2/build/unitree_lidar_ros2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ljhua/robocon/ROBOCON2025/Lidar_Pointlio (jinhua)/unilidar_sdk2-2.0.4/unitree_lidar_ros2/src/unitree_lidar_ros2
+# Build directory: /home/ljhua/robocon/ROBOCON2025/Lidar_Pointlio (jinhua)/unilidar_sdk2-2.0.4/unitree_lidar_ros2/build/unitree_lidar_ros2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
